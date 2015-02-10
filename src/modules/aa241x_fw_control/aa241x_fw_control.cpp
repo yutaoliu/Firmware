eabc11f8cd252c687197ec1d8b20943e6eb49424
Adrien Perkins <adrienp@stanford.edu>
2015-02-09 18:19:47 -0800

    beginning of skeleton for custom control law to be completed

---
+++ src/modules/aa241x_fw_control/aa241x_fw_control.cpp
@@ -0,0 +1,17 @@
+/*
+ * aa241x_fw_control.cpp
+ *
+ *  Created on: Feb 9, 2015
+ *      Author: adrienp
+ */
+
+// include header file
+#include "aa241x_fw_control.h"
+
+
+void flight_control() {
+
+	// TODO: write all of your flight control here...
+
+
+}
---
+++ src/modules/aa241x_fw_control/aa241x_fw_control.h
@@ -0,0 +1,25 @@
+/*
+ * aa241x_fw_control_main.h
+ *
+ *  Created on: Feb 9, 2015
+ *      Author: adrienp
+ */
+
+#ifndef AA241X_FW_CONTROL_MAIN_H_
+#define AA241X_FW_CONTROL_MAIN_H_
+
+
+
+
+/**
+ * Main function in which your code should be written.
+ *
+ * This is the only function that is executed at a set interval,
+ * feel free to add all the function you'd like, but make sure all
+ * the code you'd like executed on a loop is in this function.
+ */
+void flight_control();
+
+
+
+#endif /* AA241X_FW_CONTROL_MAIN_H_ */
