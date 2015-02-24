36c5e308d96b53ebf2fac0b0da0c8e746b85d3a9
Adrien Perkins <adrienp@stanford.edu>
2015-02-23 23:15:36 -0800

    adding mission definition parameters to the parameter list

--- src/modules/aa241x_mission/aa241x_mission_params.c
+++ src/modules/aa241x_mission/aa241x_mission_params.c
@@ -12,22 +12,73 @@
 /**
  * Minimum allowed altitude during mission in [ft] above Lake Lag.
  * Also acts at the mission start threshold altitude.
+ *
+ * @unit feet
+ * @group AA241x Mission
  */
 PARAM_DEFINE_FLOAT(AAMIS_MIN_ALT, 100.0f);
 
 /**
  * Maximum allowed altitude during mission  in [ft] above Lake Lag.
+ *
+ * @unit feet
+ * @group AA241x Mission
  */
 PARAM_DEFINE_FLOAT(AAMIS_MAX_ALT, 400.0f);
 
 /**
+ * Maximum allowed radial distance in [ft] from the center of the lake.
+ *
+ * @unit ft
+ * @group AA241x Mission
+ */
+PARAM_DEFINE_FLOAT(AAMIS_MAX_RAD, 300.0f);
+
+/**
  * Altitude in [ft] by which autopilot needs to be engaged in order
  * to be eligible for a mission start.
+ *
+ * @unit feet
+ * @group AA241x Mission
  */
 PARAM_DEFINE_FLOAT(AAMIS_AUTO_ALT, 50.0f);
 
 /**
  * Total duration in [mins] that the fire will spread for once
  * mission clock starts.
+ *
+ * @unit minutes
+ * @group AA241x Mission
  */
 PARAM_DEFINE_FLOAT(AAMIS_DURATION, 8.0f);
+
+/**
+ * Timestep in [sec] for each propagation of the fire.
+ *
+ * @unit seconds
+ * @group AA241x Mission
+ */
+PARAM_DEFINE_FLOAT(AAMIS_TSTEP, 10.0f);
+
+/**
+ * Standard deviation for fire propagation.
+ *
+ * @group AA241x Mission
+ */
+PARAM_DEFINE_FLOAT(AAMIS_STD, 0.35f);
+
+/**
+ * Timestep in [sec] for each propagation of the fire.
+ *
+ * @unit seconds
+ * @group AA241x Mission
+ */
+PARAM_DEFINE_FLOAT(AAMIS_TSTEP, 10.0f);
+
+/**
+ * Index (0 based) of the mission to be executed.
+ *
+ * @min 0
+ * @group AA241x Mission
+ */
+PARAM_DEFINE_INT32(AAMIS_INDEX, 0);
