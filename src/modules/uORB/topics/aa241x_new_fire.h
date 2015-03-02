dff8351d0b6fdb47c88c54e876ec0fbdf7769771
Adrien Perkins <adrienp@stanford.edu>
2015-03-01 23:35:04 -0800

    remove use of std vector and use pointers for the arrays of information. Allows handling of these messages in the logging module which seems to compile in C, not C++

--- src/modules/uORB/topics/aa241x_new_fire.h
+++ src/modules/uORB/topics/aa241x_new_fire.h
@@ -11,13 +11,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <vector>
+// #include <vector>
 #include "../uORB.h"
 
 struct aa241x_new_fire_s {
 	uint64_t mission_time;		/**< the current mission time in microseconds */
-	std::vector<int> i_new;		/**< list of i coords of the new cells */
-	std::vector<int> j_new;		/**< list of the j coords of the new cells */
+	int num_new;
+	int *i_new;
+	int *j_new;
+
+	// std::vector<int> i_new;		/**< list of i coords of the new cells */
+	// std::vector<int> j_new;		/**< list of the j coords of the new cells */
 };
 
 /* register this as object request broker structure */
--- src/modules/uORB/topics/aa241x_picture_result.h
+++ src/modules/uORB/topics/aa241x_picture_result.h
@@ -11,7 +11,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <vector>
 #include "../uORB.h"
 
 struct picture_result_s {
@@ -22,9 +21,13 @@ struct picture_result_s {
 	float center_d;			/**< Down coordinate (negative vertical distance from bottom) of the center of the picture */
 	float pic_d;			/**< diameter in [m] of the picture taken */
 	int num_cells;			/**< number of grid cells in view (length of vectors i, j, and state) */
-	std::vector<int> i;		/**< list of i grid coordinates in view */
-	std::vector<int> j;		/**< list of j grid coordinates in view */
-	std::vector<int> state; /**< state of each grid coordinate in view (-1 = water, 0 = nothing, 1 = on fire) */
+	int *i;
+	int *j;
+	int *state;
+
+	// std::vector<int> i;		/**< list of i grid coordinates in view */
+	// std::vector<int> j;		/**< list of j grid coordinates in view */
+	// std::vector<int> state; /**< state of each grid coordinate in view (-1 = water, 0 = nothing, 1 = on fire) */
 };
 
 /* register this as object request broker structure */
